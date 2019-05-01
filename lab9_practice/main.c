b0264818f563959d22482b36b4bb80c6523fffd1
Taekmin Kim <tantara.tm@gmail.com>
2019-05-01 20:42:18 +0900

    Update lab9_practice

--- lab9_practice/main.c
+++ lab9_practice/main.c
@@ -4,88 +4,34 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 
-
-#define SIZE 64
-
-typedef union {
-  float f;
-  unsigned int i;
-}foo;
+#define SIZE 4
 
 int main(int argc, char** argv)
 {
-  int i, j;
-  foo container;
+  int i;
 
-  float flat[SIZE * (SIZE + 1)];
-  float mat[SIZE][SIZE];
-  float input[SIZE];
-  float output[SIZE];
-  float output_fpga[SIZE];
+  int foo = open("/dev/mem", O_RDWR);
+  int *fpga_bram = mmap(NULL, SIZE * sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED, foo, 0x40000000);
+  int *fpga_ip   = mmap(NULL, sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED, foo, 0x43C00000);
 
-  // initialization
-  FILE *fd;
-  fd = fopen("./input.txt", "r");
-
-  unsigned int a;
-  i = 0;
-  while ( !feof(fd) )
-  {
-    if (fscanf(fd, "%X", &a) != EOF)
-    {
-      container.i = a;
-      //printf("%f %X\n", container.f, container.i);
-      if (i < SIZE)
-        input[i] = container.f;
-      else
-        mat[i % 64][i/64 - 1] = container.f;
-
-      flat[i] = container.f;
-      //printf("%d, %f\n", i, container.f);
-      i++;
-    }
-  }
-  fclose(fd);
-
+  // initialize memory
   for (i = 0; i < SIZE; i++)
-    output[i] = 0.0f;
+    *(fpga_bram + i) = (i * 2); 
+  for (i = SIZE; i < SIZE * 2; i++)
+    *(fpga_bram + i) = 0.0f; 
 
-  // computation
-  for (i = 0; i < SIZE; i++)
-    for (j = 0; j < SIZE; j++)
-      output[i] += input[SIZE-1-j] * mat[SIZE-1-j][i];
+  printf("%-10s%-10s\n", "addr", "FPGA(hex)");
+  for (i = 0; i < SIZE * 2; i++)
+    printf("%-10d%-10X\n", i, *(fpga_bram + i));
 
-  // FPGA offloading
-  // memory load
-  int foo;
-  foo = open("/dev/mem", O_RDWR | O_NONBLOCK);
-  float *fpga_bram = mmap(NULL, (SIZE * (SIZE + 1))* sizeof(float), PROT_WRITE, MAP_SHARED, foo, 0x40000000);
-  for (i = 0; i < SIZE * (SIZE + 1); i++)
-  {
-    *(fpga_bram + i) = flat[i];
-  }
-
-  // run
-  unsigned int *fpga_ip = mmap(NULL, sizeof(float), PROT_WRITE, MAP_SHARED, foo, 0x43c00000);
-  *fpga_ip = 0x5555;
-  
-  // wait
+  // run ip
+  *(fpga_ip) = 0x5555;
   while (*fpga_ip == 0x5555);
 
-  // result
-  for (i = 0; i < SIZE; i++)
-    output_fpga[i] = *(fpga_bram + i); 
-
-
-  // display
-  printf("%-10s%-10s%-10s%-10s\n", "index", "CPU", "FPGA", "FPGA(hex)");
-  for (i = 0; i < SIZE; i++)
-  {
-    container.f = output_fpga[i];
-    printf("%-10d%-10f%-10f%-10X\n", i, output[i], output_fpga[i], container.i);
-  }
-
-  close(foo);
+  printf("%-10s%-10s\n", "addr", "FPGA(hex)");
+  for (i = 0; i < SIZE * 2; i++)
+    printf("%-10d%-10X\n", i, *(fpga_bram + i));
 
   return 0;
 }
+
